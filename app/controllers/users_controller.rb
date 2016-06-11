8c1bb068693c1517c24da90ae1ec88c24cfecbcb
celiga <celine.garel@yahoo.fr>
2016-06-11 17:22:34 +0200

    added users page + url path refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def index
+    if @current_user.try(:role) != "admin"
+      flash[:error] = "Accès interdit"
+      return redirect_to request.referrer || users_home_path
+    else
+      @users = User.all
+    end
+  end
+  
   def home
   end
 
@@ -8,7 +17,7 @@ class UsersController < ApplicationController
   def logout
     session[:user_id] = nil
     flash[:info] = "Vous êtes maintenant déconnecté."
-    redirect_to "/users/home"
+    redirect_to users_home_path
   end
 
   def check
@@ -16,11 +25,11 @@ class UsersController < ApplicationController
     if @current_user
       session[:user_id] = @current_user.id
       flash[:info] = "Bienvenue #{@current_user.name} !"
-      redirect_to "/users/home"
+      redirect_to users_home_path
     else
       session[:user_id] = nil
       flash[:info] = "Échec de la connexion"
-      redirect_to "/users/login"
+      redirect_to users_login_path
     end
   end
 end
